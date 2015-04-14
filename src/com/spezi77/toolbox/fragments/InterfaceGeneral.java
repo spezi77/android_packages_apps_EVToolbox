3d26ecc9f1eab5c60cb34a26f872800f38f438df
spezi77 <spezi77@gmx.com>
2015-04-14 12:28:39 +0200

    Housekeeping * remove unused wake toggles (trackball, home) * remove lockscreen_music_controls_volbtn
    Change-Id: Ic51cfc16fb558622caa60ba798a9d4ab136e9449

--- src/com/spezi77/toolbox/fragments/InterfaceGeneral.java
+++ src/com/spezi77/toolbox/fragments/InterfaceGeneral.java
@@ -43,19 +43,13 @@ public class InterfaceGeneral extends PreferenceFragment implements
     private static final String TAG = "Toolbox";
 
     private static final String DENSITY_PICKER_PREF = "pref_interface_density_picker";
-    private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
     private static final String VOLUME_WAKE_TOGGLE = "pref_volume_wake_toggle";
-    private static final String HOME_WAKE_TOGGLE = "pref_home_wake_toggle";
-    private static final String LOCKSCREEN_MUSIC_CTRL_VOLBTN = "pref_lockscreen_music_controls_volbtn";
     private static final String FANCY_UI = "pref_interface_fancy_ui";
 
     private static final int MIN_DENSITY_VALUE = DisplayMetrics.DENSITY_LOW;
     private static final int MAX_DENSITY_VALUE = DisplayMetrics.DENSITY_XXXHIGH; //4k
 
-    private SwitchPreference mTrackballWake;
     private SwitchPreference mVolumeWake;
-    private SwitchPreference mHomeWake;
-    private SwitchPreference mMusicCtrlVolBtn;
     private SwitchPreference mFancyUi;
     private NumberPickerPreference mDensityPicker;
     private ContentResolver mCr;
@@ -73,36 +67,10 @@ public class InterfaceGeneral extends PreferenceFragment implements
 
         mCr = getActivity().getContentResolver();
 
-        /* Trackball wake pref */
-        mTrackballWake = (SwitchPreference) mPrefSet.findPreference(
-                TRACKBALL_WAKE_TOGGLE);
-        mTrackballWake.setChecked(Settings.System.getInt(mCr,
-                Settings.System.TRACKBALL_WAKE_SCREEN, 1) == 1);
-
-        /* Remove mTrackballWake on devices without trackballs */
-        if (!getResources().getBoolean(R.bool.has_trackball)) {
-            mPrefSet.removePreference(mTrackballWake);
-        }
-
         /* Volume wake pref */
         mVolumeWake = (SwitchPreference) mPrefSet.findPreference(VOLUME_WAKE_TOGGLE);
         mVolumeWake.setChecked(Settings.System.getInt(mCr,
                 Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
-
-        /* Home wake pref */
-        mHomeWake = (SwitchPreference) mPrefSet.findPreference(HOME_WAKE_TOGGLE);
-        mHomeWake.setChecked(Settings.System.getInt(mCr,
-                Settings.System.HOME_WAKE_SCREEN, 0) == 1);
-
-        /* Only show mHomeWake if the device has hardware buttons */
-        if (!getResources().getBoolean(R.bool.has_physical_buttons)) {
-            mPrefSet.removePreference(mHomeWake);
-        }
-
-        /* Volume button music controls */
-        mMusicCtrlVolBtn = (SwitchPreference) mPrefSet.findPreference(LOCKSCREEN_MUSIC_CTRL_VOLBTN);
-        mMusicCtrlVolBtn.setChecked(Settings.System.getInt(mCr,
-                Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN, 1) == 1);
 
         mFancyUi = (SwitchPreference) mPrefSet.findPreference(FANCY_UI);
         mFancyUi.setChecked(ActivityManager.isForcedHighEndGfx());
@@ -145,24 +113,9 @@ public class InterfaceGeneral extends PreferenceFragment implements
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         boolean value;
 
-        if (preference == mTrackballWake) {
-            value = mTrackballWake.isChecked();
-            Settings.System.putInt(mCr, Settings.System.TRACKBALL_WAKE_SCREEN,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mVolumeWake) {
+	if (preference == mVolumeWake) {
             value = mVolumeWake.isChecked();
             Settings.System.putInt(mCr, Settings.System.VOLUME_WAKE_SCREEN,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mHomeWake) {
-            value = mHomeWake.isChecked();
-            Settings.System.putInt(mCr, Settings.System.HOME_WAKE_SCREEN,
-                    value ? 1 : 0);
-            return true;
-        } else if (preference == mMusicCtrlVolBtn) {
-            value = mMusicCtrlVolBtn.isChecked();
-            Settings.System.putInt(mCr, Settings.System.LOCKSCREEN_MUSIC_CONTROLS_VOLBTN,
                     value ? 1 : 0);
             return true;
         } else if (preference == mFancyUi) {
